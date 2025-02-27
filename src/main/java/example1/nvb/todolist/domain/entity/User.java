16e5ea17af6da2c7b71276f4e8e89caaef1a0b62
myrdin44 <nv.bac44@gmial.com>
2025-02-27 15:34:01 +0700

    first commit

---
+++ src/main/java/example1/nvb/todolist/domain/entity/User.java
@@ -0,0 +1,50 @@
+package example1.nvb.todolist.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.springframework.beans.factory.annotation.Value;
+import vn.saolasoft.core.domain.entity.FullAuditedEntity;
+
+@Entity
+@Table(name = "users")
+public class User extends FullAuditedEntity<String, String> {
+
+    @Value("${user.username}")
+    private String username;
+    @Value("${user.password}")
+    private String password;
+
+    private String email;
+
+    public User() {}
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
